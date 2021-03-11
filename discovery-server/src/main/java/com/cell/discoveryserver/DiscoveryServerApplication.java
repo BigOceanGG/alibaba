014e49ef2e26c9e64e9ccadbf6cbfb36288ab8b0
bigocean <3408598@qq.com>
2021-03-11 14:46:24 +0800

    add interface

--- discovery-server/src/main/java/com/cell/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/cell/discoveryserver/DiscoveryServerApplication.java
@@ -2,12 +2,27 @@ package com.cell.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+    @Slf4j
+    @RestController
+    static class TestController {
+        @GetMapping("/hello")
+        public String hello(@RequestParam String name) {
+            log.info("invoked name = " + name);
+            return "hello " + name;
+        }
     }
 
 }
