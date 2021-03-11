1410adb1ec07235df6ed3dc9dd85c898e095b6f5
bigocean <3408598@qq.com>
2021-03-11 17:36:50 +0800

    add feign

---
+++ discovery-client-feign/src/main/java/com/cell/discoveryclientfeign/DiscoveryClientFeignApplication.java
@@ -0,0 +1,43 @@
+package com.cell.discoveryclientfeign;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableFeignClients
+public class DiscoveryClientFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryClientFeignApplication.class, args);
+    }
+
+    @Slf4j
+    @RestController
+    static class TestController {
+
+        @Autowired
+        Client client;
+
+        @GetMapping("/test")
+        public String test() {
+            String result = client.hello("feign");
+            return "Return : " + result;
+        }
+    }
+
+    @FeignClient("discovery-server")
+    interface Client {
+
+        @GetMapping("/hello")
+        String hello(@RequestParam(name = "name") String name);
+
+    }
+
+}
---
+++ discovery-client-feign/src/test/java/com/cell/discoveryclientfeign/DiscoveryClientFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cell.discoveryclientfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryClientFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
