371e2a90f27a91c58759ac0af6d50f7c12d340c0
bigocean <3408598@qq.com>
2021-03-22 11:15:47 +0800

    add sentinel-annotation

---
+++ sentinel-annotation/src/main/java/com/cell/sentinelannotation/SentinelAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.cell.sentinelannotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelAnnotationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelAnnotationApplication.class, args);
+    }
+
+}
---
+++ sentinel-annotation/src/test/java/com/cell/sentinelannotation/SentinelAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cell.sentinelannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
