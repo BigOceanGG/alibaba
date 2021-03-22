614f34dec2c88666f23d4a69d2018ebd5bc11c71
bigocean <3408598@qq.com>
2021-03-22 11:34:33 +0800

    modify sentinel-annotation

--- sentinel-annotation/src/main/java/com/cell/sentinelannotation/SentinelAnnotationApplication.java
+++ sentinel-annotation/src/main/java/com/cell/sentinelannotation/SentinelAnnotationApplication.java
@@ -1,7 +1,9 @@
 package com.cell.sentinelannotation;
 
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SentinelAnnotationApplication {
@@ -10,4 +12,8 @@ public class SentinelAnnotationApplication {
         SpringApplication.run(SentinelAnnotationApplication.class, args);
     }
 
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect() {
+        return new SentinelResourceAspect();
+    }
 }
